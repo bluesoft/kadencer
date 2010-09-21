b4517693c299a7a33c1453449ced53f91252a828
luizfaias <luizfaias@gmail.com>
2010-09-21 07:57:39 -0300

    Starting rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kadencer::Application.config.secret_token = '88a872d72ba01b2890352f877fa07af4e596956b627c3966d59e6699e1fb6a163313f147dfe2a06f41c0393a163e8cdae8008a325f0a62b21c7bacdd588ae411'
